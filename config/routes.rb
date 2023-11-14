cfe3abd3b3ea84173c332effd52fdb3dce4c88da
Charlotte De Baere <dbr.charlotte@gmail.com>
2023-11-14 16:18:11 +0000

    C3 - Rails Wikinimous

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  root to: 'articles#index'
+  get 'articles', to: 'articles#index'
+  get 'articles/new', to: 'articles#new'
+  get 'articles/:id', to: 'articles#show', as: 'article'
+  post 'articles', to: 'articles#create'
+  get 'articles/:id/edit', to: 'articles#edit', as: 'article_edit'
+  patch 'articles/:id', to: 'articles#update'
+  delete 'articles/:id', to: 'articles#destroy', as: 'article_destroy'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+10.times do
+  title = Faker::Book.title
+  content = Faker::Quote.famous_last_words
+  Article.create(title: title, content: content)
+end
